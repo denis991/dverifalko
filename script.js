a715092e3ae20392227fe6c41851679cb43d490f
Denis Gutsulak <d.99113@gmail.com>
2023-06-21 15:28:28 +0300

    Jean

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 //Аксессуары
+
 const accessoriesSelect = document.getElementById("accessories");
 const wrapperSelect = document.getElementById("select-wrapper");
 const selectedAccessoriesInput = document.getElementById("selected-accessories");
 const showBtn = document.getElementById("show-btn");
 const hideBtn = document.getElementById("hide-btn");
-
-showBtn.addEventListener("click", showOptions);
-hideBtn.addEventListener("click", hideOptions);
-accessoriesSelect.addEventListener("change", updateSelectedAccessories);
 
 function showOptions() {
   wrapperSelect.classList.add("show-dropdown");
@@ -22,7 +19,11 @@ function updateSelectedAccessories() {
   selectedAccessoriesInput.value = selectedOptions.join(", ");
 }
 
-//door двери 
+showBtn.addEventListener("click", showOptions);
+hideBtn.addEventListener("click", hideOptions);
+accessoriesSelect.addEventListener("change", updateSelectedAccessories);
+
+//door двери
 // Получаем элементы управления параметрами
 const colorPaintSelect = document.getElementById('color-paint');
 const colorFilmSelect = document.getElementById('color-film');
@@ -52,5 +53,45 @@ colorPaintSelect.addEventListener('change', handleParameterChange);
 colorFilmSelect.addEventListener('change', handleParameterChange);
 colorHandleSelect.addEventListener('change', handleParameterChange);
 openingSelect.addEventListener('change', handleParameterChange);
+//v2
+/**   // Функция для применения выбранных параметров к визуальной части двери
+function applyDoorStyle() {
+  var outerDoor = document.getElementById("outer-door");
+  var innerDoor = document.getElementById("inner-door");
+  var colorPaint = document.getElementById("color-paint").value;
+  var colorFilm = document.getElementById("color-film").value;
+  var colorHandle = document.getElementById("color-handle").value;
+  var opening = document.getElementById("opening").value;
 
+  outerDoor.style.backgroundColor = colorPaint;
+  outerDoor.style.border = "2px solid " + colorFilm;
+  innerDoor.style.backgroundColor = colorPaint;
+  innerDoor.style.border = "2px solid " + colorFilm;
+
+  if (opening === "left") {
+    outerDoor.classList.remove("mirror");
+    innerDoor.classList.add("mirror");
+  } else {
+    outerDoor.classList.add("mirror");
+    innerDoor.classList.remove("mirror");
+  }
+
+  var doorHandle = document.getElementsByClassName("door-handle")[0];
+  doorHandle.style.backgroundColor = colorHandle;
+}
+
+// Обработчик события для изменения параметров двери
+function handleParameterChange() {
+  applyDoorStyle();
+}
+
+// Назначение обработчика события для изменения параметров
+var selects = document.getElementsByTagName("select");
+for (var i = 0; i < selects.length; i++) {
+  selects[i].addEventListener("change", handleParameterChange);
+}
+
+// Начальное применение параметров
+applyDoorStyle();
+  */
 // end door
